15a6b993d0628f74467b6ed1af3f3ce063ab75e5
herminioaureo <herminio.aureo@outlook.com>
2025-02-01 11:34:19 -0300

    wip: add glucose page

--- bloodglucoseapp/src/app/addglucose/addglucose.component.ts
+++ bloodglucoseapp/src/app/addglucose/addglucose.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import { MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatCardActions } from '@angular/material/card';
 
 @Component({
   selector: 'app-addglucose',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, 
+           MatToolbarModule, 
+           MatIconModule, 
+           MatCardModule, 
+           MatDialogModule, 
+           MatButtonModule, 
+           FormsModule, 
+           MatFormFieldModule,
+           MatInputModule, 
+           MatDividerModule, 
+           MatCardActions,
+           MatSelectModule],
   templateUrl: './addglucose.component.html',
   styleUrl: './addglucose.component.scss'
 })
 export class AddglucoseComponent {
+
+  saveGlucose(meal: string, value: string, date: string) {
+    console.log("Vai salvar a glicemia ->" + meal + value + date);
+    
+  }
 
 }
--- bloodglucoseapp/src/app/app.component.ts
+++ bloodglucoseapp/src/app/app.component.ts
@@ -1,32 +1,77 @@
-import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import { MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { AddglucoseComponent } from './addglucose/addglucose.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardActions } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatCardModule, MatButtonModule],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatCardModule, MatDialogModule, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'bloodglucoseapp';
 
   constructor(private router: Router) {}
 
-
   logout() {
-    console.log('Logout. Token antes do Logout: ' + localStorage.getItem('token'));
-    console.log('Logout. username apos o Logout: ' + localStorage.getItem('username'));
     localStorage.removeItem('token');
-    console.log('Logout. Token apos o Logout: ' + localStorage.getItem('token'));
     localStorage.removeItem('username');
-    console.log('Logout. username apos o Logout: ' + localStorage.getItem('username'));
     this.router.navigate(['/login']);
+  }
 
+  readonly dialog = inject(MatDialog);
+
+  openDialog() {
+    let token =  localStorage.getItem('token');
+    if (token != null) {
+      const dialogRef = this.dialog.open(DialogContentExampleDialog);
+      dialogRef.afterClosed().subscribe(result => {
+        console.log(`Dialog result: ${result}`);
+      });
+    } else {
+      console.log("token ausente");
+    }
+  }
+}
+
+@Component({
+  selector: 'dialog-content-example-dialog',
+  standalone: true,
+  templateUrl: './addglucose/addglucose.component.html',
+  imports: [RouterOutlet, 
+        MatToolbarModule, 
+        MatIconModule, 
+        MatCardModule, 
+        MatDialogModule, 
+        MatButtonModule, 
+        FormsModule, 
+        MatFormFieldModule,
+        MatInputModule, 
+        MatDividerModule, 
+        MatCardActions,
+        MatSelectModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DialogContentExampleDialog {
+
+  glucoseComponent: AddglucoseComponent = new AddglucoseComponent;
+
+  saveGlucose(meal: string, value: string, date: string){
+    this.glucoseComponent.saveGlucose(meal, value, date);
   }
 
 }
